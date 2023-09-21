311ba5e72c3eff054f94c95b79259be277e651d5
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-09-21 23:20:56 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,61 @@
 import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillBriefcaseFill} from 'react-icons/bs'
 import {FiLogOut} from 'react-icons/fi'
 import './index.css'
 
-const Header = () => (
-  <>
-    <div className="nav-sm-header">
-      <img
-        className="home-logo-image"
-        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-        alt="website logo"
-      />
-      <div className="nav-header-elements">
-        <AiFillHome className="headers-icon" />
-        <BsFillBriefcaseFill className="headers-icon" />
-        <FiLogOut className="headers-icon" />
+const Header = props => {
+  const {history} = props
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+  return (
+    <>
+      <div className="nav-sm-header">
+        <img
+          className="home-logo-image"
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+        />
+        <div className="nav-header-elements">
+          <Link to="/">
+            <AiFillHome className="headers-icon" />
+          </Link>
+          <Link to="/jobs">
+            <BsFillBriefcaseFill className="headers-icon" />
+          </Link>
+          <button
+            type="button"
+            className="logout-icon-button"
+            onClick={onClickLogout}
+          >
+            <FiLogOut className="headers-icon" />
+          </button>
+        </div>
       </div>
-    </div>
 
-    <div className="nav-lg-header">
-      <img
-        className="home-logo-image"
-        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-        alt="website logo"
-      />
-      <div className="nav-lg-elements">
-        <p className="nav-element">Home</p>
-        <p className="nav-element">Jobs</p>
+      <div className="nav-lg-header">
+        <img
+          className="home-logo-image"
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+        />
+        <div className="nav-lg-elements">
+          <Link to="/" className="nav-element">
+            <p>Home</p>
+          </Link>
+          <Link to="/jobs" className="nav-element">
+            <p>Jobs</p>
+          </Link>
+        </div>
+        <button type="button" className="logout-button" onClick={onClickLogout}>
+          Logout
+        </button>
       </div>
-      <button type="button" className="logout-button">
-        Logout
-      </button>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default withRouter(Header)
